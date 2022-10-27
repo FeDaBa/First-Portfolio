cb53f59f0be6e78346e000b9407cd4773c823751
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-27 20:52:26 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,9 @@ import { Link } from 'react-scroll'
 
 
 const Home = () => {
+  
   return (
+    
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
 
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
@@ -42,13 +44,17 @@ const Home = () => {
             </div>
 
             <div>
+                              
                 <img src={heroImage} alt="my profile" className="rounded-full mx-auto w-2/3 md:w-full"/>
+                  
             </div>
 
         </div>
 
     </div>
+
   )
+  
 }
 
 export default Home
