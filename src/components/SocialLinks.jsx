b76e28d804ba6318c3f6d9b8d0337fe3e6af0efe
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-27 20:37:56 -0300

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -53,11 +53,11 @@ const SocialLinks = () => {
 
         <ul>
 
-            {links.map(({ id, child, href, style, download }) => (
+            {links.map(({ id, child, href, download }) => (
 
                 <li
                     key={id}
-                    className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300" + " " + style}>
+                    className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300"}>
 
                     <a
                         href={href}
