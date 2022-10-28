925f0999dbe3ea50723ef9a92afd7896ab242b74
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-27 22:39:48 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,19 +19,36 @@ const About = () => {
             </p>
 
             <br />
+            <p className="text-xl mt-20 leading-8 tracking-wide">
 
-            <p className="text-xl leading-8 tracking-wide">
-                In March 2022 I started a career as a <em>Systems Analyst</em> at the <em>National University from the Austral Patagonia</em> (UNPA),
-                where I learned a lot about programming and software engineering. Some months after, I joined a new course called <em>Argentina Programa</em>, wich is a national program
-                granted by the <em>Ministry of Production</em> and certified by the <em>National Instite of Industrial Technology</em> (INTI) and the <em>Chamber of the Argentine Software Industry</em> (CESSI)
+                I joined the world of web development in early's 2021 with a course given by <a href="https://conectaempleo-formacion.fundaciontelefonica.com/argentina" className="text-cyan-400 duration-300" target={'_blank'} rel="noreferrer"><em>Telefónica</em></a>,
+                then I started experimenting with HTML and CSS to know better how those languages works,
+                and also continued taking more web development courses by <a href="https://www.freecodecamp.org/" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em>FreeCodeCamp</em></a>,
+                <a href="https://www.udemy.com" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em> Udemy</em></a>,
+                <a href="https://www.theodinproject.com/home" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em> The Odin Project</em></a>, etc.
+
+            </p>
+
+            <br />
+
+            <p className="text-xl leading-8 tracking-wide font">
+
+                In March 2022 I started studing the career of <em>Systems Analyst</em> at the <a href="https://www.uarg.unpa.edu.ar/" className="text-cyan-400 duration-300" target={'_blank'} rel="noreferrer"><em>Universidad Nacional de la Patagonia Austral</em></a> (UNPA),
+                where I learned a lot about programming and software engineering. Some months after, I joined a new course called <em>Argentina Programa</em>, wich is a national program to learn web development,
+                granted by the <a href="https://www.argentina.gob.ar/produccion" className="text-cyan-400 duration-300" target={'_blank'} rel="noreferrer"><em>Ministerio de producción </em></a>
+                and certified by the <a href="https://www.argentina.gob.ar/inti" className="text-cyan-400 duration-300" target={'_blank'} rel="noreferrer"><em>Instituto Nacional de Tecnología Industrial</em></a> (INTI)
+                and the <a href="https://cessi.org.ar/" className="text-cyan-400 hover:font-normal duration-300" target={'_blank'} rel="noreferrer"><em>Cámara de la Industria Argentina del Software</em></a> (CESSI)
+
             </p>
 
             <br />
 
             <p className="text-xl leading-8 tracking-wide">
+
                 In the present day, I know more than 8 programming languages, frameworks and development platforms.
-                My main is the Front End, but I'm also learning about Back End to have a better understanding about how the web based software works.
+                My specialty is the Front End, but I'm also learning about Back End to have a better understanding about how the web based software works.
                 I'm always looking to discover new horizons and improve myself with new challenges.
+
             </p>
 
         </div>
