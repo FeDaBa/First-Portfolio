7991a47e85120fe3f1235ac77658a4d065d38cce
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-27 22:45:19 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const About = () => {
+  
   return (
+    
     <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
 
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
@@ -37,7 +39,9 @@ const About = () => {
         </div>
 
     </div>
+
   )
+  
 }
 
 export default About
