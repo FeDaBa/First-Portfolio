e387f4e2cbb36290e22ff06eb7717ed6c2b9109c
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-28 16:25:20 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
 
-            <div className="flex flex-col justify-center">
+            <div className="flex flex-col justify-between">
 
                 <h2 className="text-4xl sm:text-7xl font-bold text-white">
                     I'm a Front End Developer
@@ -46,7 +46,7 @@ const Home = () => {
 
              <div>
 
-                <img src={heroImage} alt="my profile" className="rounded-full mx-auto w-2/3 md:w-full mt-6 md:mt-0"/>
+                <img src={heroImage} alt="my profile" className="rounded-full mx-auto w-2/3 md:w-full"/>
 
              </div>
 
