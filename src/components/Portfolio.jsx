af80a5d6b24a9bcb18f00b17ceffa70c9c99d0ea
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-28 14:32:07 -0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -53,7 +53,7 @@ const Portfolio = () => {
 
     <div
         name="portfolio"
-        className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+        className="bg-gradient-to-b from-black to-gray-800 w-full text-white lg:h-screen"
     >
 
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
