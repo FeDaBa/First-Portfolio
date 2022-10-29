42b18eaa7100ad54b9ca5153cabfd4105d1489a7
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-29 18:17:47 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <div className="pb-8">
 
                 <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-                    Sobre mi
+                    Acerca de mi
                 </p>
 
             </div>
