5315b76b52f48547b8e97d12eac2071fc024ce1f
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-29 18:21:10 -0300

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -76,7 +76,7 @@ const Experience = () => {
             <div>
 
                 <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
-                    Experience
+                    Experiencia
                 </p>
 
                 <p className="py-6">
