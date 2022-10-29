f7f6036d4ebed1c7021c8f3b1f9f93fac753e9b6
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-10-29 17:47:58 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
                 </h2>
 
                 <p className="text-gray-500 py-4 max-w-md">
-                     Hi! I am Federico. Welcome to my portfolio.
+                     Mi nombre es Federico, soy desarrollador web desde 2021.
                 </p>
 
                 <div>
