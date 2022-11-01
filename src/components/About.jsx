26d3c4fcc374b38530d6bbc508516451a443c3bb
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-11-01 17:42:28 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,8 +19,8 @@ const About = () => {
             <p className="text-xl leading-8 tracking-wide">
 
                 En Marzo de 2022 comencé a cursar la carrera <em>Analista de Sistemas</em> en la <a href="https://www.uarg.unpa.edu.ar/" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em>Universidad Nacional de la Patagonia Austral</em></a> (UNPA),
-                a traves de la cual he aprendido una gran cantidad de conceptos y fundamentos sobre Programación e Ingeniería de Software. 
-                
+                a traves de la cual he aprendido una gran cantidad de conceptos y fundamentos sobre Programación e Ingeniería de Software. <br />
+       
                 Algunos meses despues, comencé a cursar el plan <em>Argentina Programa</em>, 
                 un extenso curso de 7 meses de duración enfocado en el Desarrollo Web Full Stack, el cual se encuentra certificado por el 
                 <a href="https://www.argentina.gob.ar/inti" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em> Instituto Nacional de Tecnología Industrial</em></a> (INTI)
