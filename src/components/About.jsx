a31373e26acf370dca799f80bdf6717e8fafc1c2
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-11-01 17:32:31 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,9 +18,9 @@ const About = () => {
 
             <p className="text-xl leading-8 tracking-wide font">
 
-                Empecé a cursar la carrera de <em>Analista de Sistemas</em> en la <a href="https://www.uarg.unpa.edu.ar/" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em>Universidad Nacional de la Patagonia Austral</em></a> (UNPA),
-                a traves de la cual he aprendido una gran cantidad de conceptos y fundamentos sobre Programación e Ingeniería de Software. Algunos meses despues comencé a cursar el plan <em>Argentina Programa</em>, 
-                un extenso curso sobre Desarrollo Web Full Stack certificado por el 
+                En Marzo de 2022 comencé a cursar la carrera <em>Analista de Sistemas</em> en la <a href="https://www.uarg.unpa.edu.ar/" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em>Universidad Nacional de la Patagonia Austral</em></a> (UNPA),
+                a traves de la cual he aprendido una gran cantidad de conceptos y fundamentos sobre Programación e Ingeniería de Software. Algunos meses despues, comencé a cursar el plan <em>Argentina Programa</em>, 
+                un extenso curso de 7 meses de duración enfocado en el Desarrollo Web Full Stack, el cual se encuentra certificado por el 
                 <a href="https://www.argentina.gob.ar/inti" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em> Instituto Nacional de Tecnología Industrial</em></a> (INTI)
                 y la <a href="https://cessi.org.ar/" className="text-cyan-400" target={'_blank'} rel="noreferrer"><em> Cámara de la Industria Argentina del Software</em></a> (CESSI)
 
