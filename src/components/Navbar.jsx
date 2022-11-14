ae4b7217f4269e2d040a5cb8d003301d7cf39e4a
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-11-14 18:42:26 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
 
         <div
             onClick={() => setNav(!nav)}
-            className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+            className="cursor-pointer mr-4 z-10 text-gray-500 md:hidden"
         >
             {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
 
@@ -64,7 +64,7 @@ const Navbar = () => {
 
         {nav && (
 
-        <ul className={`flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 transition-all duration-200 ${setNav ? "left-0" : "-left-full"}`}>
+        <ul className={`flex flex-col justify-center items-center fixed top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 transition-all duration-200 ${setNav ? "left-0" : "-left-full"}`}>
 
             {links.map(({ id, link }) => (
 
