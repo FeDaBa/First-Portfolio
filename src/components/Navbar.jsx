d07f0692eee2e51fd53dfe11fc87efbb4772b170
Federico Bahamonde <94578149+FeDaBa@users.noreply.github.com>
2022-11-14 18:40:49 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
 
         {nav && (
 
-        <ul className={`flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 transition-all duration-200 ${showMenu ? "left-0" : "-left-full"}`}>
+        <ul className={`flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 transition-all duration-200 ${setNav ? "left-0" : "-left-full"}`}>
 
             {links.map(({ id, link }) => (
 
